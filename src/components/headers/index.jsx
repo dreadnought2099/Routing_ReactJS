e09f8156a9a1c4671ae0fa20954f29cdee87e09d
Raymart Magallanes <raymart.magallanes@bisu.edu.ph>
2024-11-23 14:21:17 +0800

    fix the headers and add bootstrap cdn

--- src/components/headers/index.jsx
+++ src/components/headers/index.jsx
@@ -2,12 +2,18 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <>
-      <div className="header">
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/contact">Contact</Link>
+    <nav className="navbar navbar-dark bg-dark">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">
+          Home
+        </Link>
+        <Link className="navbar-brand" to="/about">
+          About
+        </Link>
+        <Link className="navbar-brand" to="/contact">
+          Contact
+        </Link>
       </div>
-    </>
+    </nav>
   );
 }
